9e9f61e5b177c374afc2427e26c5b27a366d35f1
kiraitora-dev <kiraitora@gmail.com>
2026-06-17 12:49:07 +0700

    feat: SQLAlchemy models

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,15 @@
+from app.models.category import Category
+from app.models.menu_item import MenuItem
+from app.models.order import Order
+from app.models.recommendation import Recommendation
+from app.models.restaurant import Restaurant
+from app.models.user import User
+
+__all__ = [
+    "Category",
+    "MenuItem",
+    "Order",
+    "Recommendation",
+    "Restaurant",
+    "User",
+]
